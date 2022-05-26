a4d53debce355c98e228ebf5a81710f3af67eb27
philippe1997 <philippesteve2.ps@gmail.com>
2022-05-26 15:35:55 +0100

    edit on routing strat

--- developpement/src/app/Controllers/Visite.php
+++ developpement/src/app/Controllers/Visite.php
@@ -10,24 +10,28 @@ class Visite extends BaseController
 {
     public function index()
     {
-        $visiteModel=new visiteModel();
-        $empModel=new employerModel();
-        $visiteurModel=new visiteurModel();
-        $tab=array();
-        $toShow=array();
-        $visites=$visiteModel->findAll();
+        $visiteModel = new visiteModel();
+        $empModel = new employerModel();
+        $visiteurModel = new visiteurModel();
+        $tab = array();
+        $toShow = array();
+        $visites = $visiteModel->findAll();
 
-        foreach($visites as $visite)
-        {
-            
-            $toShow['employer']=$empModel->get_attr_of((int)$visite->id_employer,"login");
-            $toShow['visiteur']=$visiteurModel->get_attr_of((int)$visite->id_visiteur,"nom")." ".$visiteurModel->get_attr_of((int)$visite->id_visiteur,"prenom");
-            $toShow['date']=$visite->date;
-            $toShow['heure_debut']=$visite->heure_debut;
-            $toShow['heure_fin']=$visite->heure_fin;
-            $toShow['raison']=$visite->raison;
-            array_push($tab, $toShow );
+        foreach ($visites as $visite) {
+
+            $toShow['employer'] = $empModel->get_attr_of((int)$visite->id_employer, "login");
+            $toShow['visiteur'] = $visiteurModel->get_attr_of((int)$visite->id_visiteur, "nom") . " " . $visiteurModel->get_attr_of((int)$visite->id_visiteur, "prenom");
+            $toShow['date'] = $visite->date;
+            $toShow['heure_debut'] = $visite->heure_debut;
+            $toShow['heure_fin'] = $visite->heure_fin;
+            $toShow['raison'] = $visite->raison;
+            array_push($tab, $toShow);
         }
-        return view('liste_visite',['visites'=>$tab]);
+        return view('listeVisiteView', ['visites' => $tab]);
+    }
+    public function test()
+    {
+        # code.
+        echo 'test';
     }
 }
--- developpement/src/app/Views/listeVisiteView.php
+++ developpement/src/app/Views/listeVisiteView.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,15 +8,16 @@
     <link rel="stylesheet" href="assets/style/dist/style.css">
     <title>Document</title>
 </head>
+
 <body>
     <div class="list">
-    
+
         <h1 class="titre-tableau"> Liste des visites</h1>
 
         <button class="open-popup btn-ajouter" popup_to_open="p_form_visite">Ajouter</button>
         <div>
-            <span >nombre d'elements: </span>
-        </div> 
+            <span>nombre d'elements: </span>
+        </div>
         <table>
             <tr>
                 <th>Visiteur</th>
@@ -27,36 +29,35 @@
                 <th>Action</th>
             </tr>
             <?php
-                foreach($visites as $visite)
-                {
-                    ?>
-                    <tr>
-                        <td><?php echo $visite['visiteur'];?></td>
-                        <td><?php echo $visite['employer'];?></td>
-                        <td><?php echo $visite['raison'];?></td>
-                        <td><?php echo $visite['date'];?></td>
-                        <td><?php echo $visite['heure_debut'];?></td>
-                        <td><?php echo $visite['heure_fin'];?></td>
-                        <td>
-                            <?php
-                            if($visite['heure_fin']=="") 
-                            {
-                                ?>
-                                <a href='<?=base_url();?>fin/'>fini</a>
-                                <?php
-                            }
-                            ?>
-                        </td>
-                    </tr>
+            foreach ($visites as $visite) {
+            ?>
+            <tr>
+                <td><?php echo $visite['visiteur']; ?></td>
+                <td><?php echo $visite['employer']; ?></td>
+                <td><?php echo $visite['raison']; ?></td>
+                <td><?php echo $visite['date']; ?></td>
+                <td><?php echo $visite['heure_debut']; ?></td>
+                <td><?php echo $visite['heure_fin']; ?></td>
+                <td>
+                    <?php
+                        if ($visite['heure_fin'] == "") {
+                        ?>
+                    <a href='<?= base_url(); ?>fin/'>fini</a>
+                    <?php
+                        }
+                        ?>
+                </td>
+            </tr>
             <?php
-                }
+            }
             ?>
         </table>
     </div>
     <div class="popup-back" popup_id="p_form_visite">
         <form action="" class="popup-form">
             <div class="relative">
-                <button type="button"><img src="assets/image/button/fermer-la-croix.png" alt="fermer" class="close-popup absolute right-0 top-0 w-4 h-4"  popup_to_close="p_form_visite" ></button>
+                <button type="button"><img src="assets/image/button/fermer-la-croix.png" alt="fermer"
+                        class="close-popup absolute right-0 top-0 w-4 h-4" popup_to_close="p_form_visite"></button>
             </div>
             <div class="form-header">
                 <h2> SEED RECEPTION</h2>
@@ -78,7 +79,7 @@
                 </div>
                 <div>
                     <label for="Tel" class="requis">Telphone</label>
-                    <input type="number" id="tel"class="textfield"  placeholder="..." required>
+                    <input type="number" id="tel" class="textfield" placeholder="..." required>
                 </div>
             </div>
 
@@ -89,7 +90,7 @@
                 </div>
                 <div>
                     <label for="hd" class="requis">Heure de debut</label>
-                    <input type="time" id="tel"class="textfield"  placeholder="..." required>
+                    <input type="time" id="tel" class="textfield" placeholder="..." required>
                 </div>
                 <div>
                     <label for="hf">Heure de fin</label>
@@ -98,7 +99,8 @@
             </div>
             <div class="my-2">
                 <label for="raison">Raison</label>
-                <textarea name="" id="" cols="20" rows="5" class="shadow appearance-none border rounded w-full py-2 px-3 focus:ring-1 focus:ring-blue-300 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
+                <textarea name="" id="" cols="20" rows="5"
+                    class="shadow appearance-none border rounded w-full py-2 px-3 focus:ring-1 focus:ring-blue-300 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
             </div>
 
             <div class="form-footer">
@@ -107,10 +109,11 @@
                     <input type="submit" value="Ajouter">
                 </div>
             </div>
-            
+
         </form>
     </div>
 
     <script src="assets/script/popup.js"></script>
 </body>
+
 </html>
